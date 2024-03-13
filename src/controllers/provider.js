8de25e63f7b8727ab9cb8969c663181020845bcc
Ali Sher <157805164+Monkey-D-Luffy-7@users.noreply.github.com>
2024-03-13 14:49:56 +0500

    specialities crud

--- src/controllers/provider.js
+++ src/controllers/provider.js
@@ -4,13 +4,6 @@ const { signupProvider } = require("../services/provider");
 const signup_provider = async (req, res) => {
   // console.log("BODYYYYYYYYY", req.body);
   try {
-    // try {
-    //   await validateProvider(req.body);
-    // } catch (e) {
-    //   return res
-    //     .status(400)
-    //     .json({ code: 400, message: e.details[0].message.replace(/\"/g, "") });
-    // }
     const { error } = validateProvider.validate(req.body);
     if (error) {
       return res.status(400).json({ error: error.details[0].message });
